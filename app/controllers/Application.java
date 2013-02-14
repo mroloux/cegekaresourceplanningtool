ee14cec999bd45bb6d9154980ab1462d10451fb5
bkchb <bkchb@HI16880.cegekanv.corp.local>
2013-02-14 13:40:01 +0100

    story aanmaken project

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,16 +1,27 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
-
-import java.util.*;
-
-import models.*;
+import models.Project;
+import play.data.validation.Required;
+import play.mvc.Controller;
 
 public class Application extends Controller {
 
     public static void index() {
         render();
+    }
+    
+    public static void voegProjectToe(@Required String naam, String locatie) {
+    	Project project = new Project();
+    	project.naam = naam;
+    	project.locatie = locatie;
+    	project.save();
+    	if(validation.hasErrors()) {
+    		params.flash();
+    		validation.keep();
+    	} else {
+    		flash.put("boodschap", "Project werd bewaard.");
+    	}
+    	index();
     }
 
 }
